937328716d738e647e01d42d7f8c84a1ea88cfc9
Fabricio <fgonzalezguasque@gmail.com>
2024-03-23 09:41:30 -0300

    Modificaciones en el circuit braeker

--- catalog-service/src/main/java/com/dh/catalogservice/repository/MovieFeignRepository.java
+++ catalog-service/src/main/java/com/dh/catalogservice/repository/MovieFeignRepository.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,6 +19,6 @@ public interface MovieFeignRepository {
     String getGreeting();
 
     @GetMapping("/api/v1/movies/findAll")
-    ResponseEntity<List<Movie>> findAllMovies(Boolean throwError);
+    List<Movie> findAllMovies(@RequestParam Boolean throwError);
 
 }
--- serie-service/src/main/java/com/dh/serieservice/SerieServiceApplication.java
+++ serie-service/src/main/java/com/dh/serieservice/SerieServiceApplication.java
@@ -5,11 +5,13 @@ import com.dh.serieservice.repository.SerieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 import java.util.UUID;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SerieServiceApplication {
 
